627c6f528910ad37f2ebbc85f932c3a5b7d1d120
Trammell Hudson <hudson@trmm.net>
2016-06-21 13:01:48 -0400

    import of code from old tree

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# lighthouse
-Arduino and Processng library to interface with the HTC Vive Lighthouse beacons
+![Two Lighthouse beacons and a Teensy on a breadboard](https://farm8.static.flickr.com/7336/27747465696_948e961a4c.jpg)
+
+Arduino (Teensy) and Processng library to interface with the HTC Vive
+Lighthouse beacons.  Not fully functional yet.
+
+More info: https://trmm.net/Lighthouse
+
+
---
+++ firmware/firmware.ino
@@ -0,0 +1,72 @@
+/** \file
+ * XYZ position using the Vive Lighthouse beacons.
+ */
+
+#define IR1 20
+
+void setup()
+{
+	pinMode(IR1, INPUT_PULLUP);
+	Serial.begin(115200);
+}
+
+
+static inline boolean read_pin()
+{
+	return bit_is_set(PINF,1);
+	//return digitalReadFast(IR1) & 2;
+}
+
+#define NUM_PULSES 64
+uint32_t pulses[NUM_PULSES];
+unsigned iter = 0;
+
+void loop()
+{
+	// collect the timings on the next set of short pulses
+	uint32_t sync_start = 0;
+
+	for(int i = 0 ; i < NUM_PULSES ; )
+	{
+		while(!read_pin()) // PORTF & (1 << 1))
+			;
+
+		uint32_t pulse_start = micros();
+
+		while(read_pin()) // !(PORTF & (1 << 1)))
+			;
+
+		uint32_t pulse_end = micros();
+		uint32_t delta = pulse_end - pulse_start;
+		if (delta > 40)
+		{
+			// sync pulse
+			sync_start = pulse_start;
+		} else {
+			// sweep pulse
+			pulses[i++] = pulse_start - sync_start;
+		}
+	}
+
+	//Serial.print(iter++);
+
+	for(int i = 0 ; i < NUM_PULSES ; i++)
+	{
+#if 0
+		char s[32];
+		sprintf(s, "%5d,%4d",
+			(int) (pulses[i] - start),
+			(int) (pulses[i+1] - pulses[i])
+		);
+		//Serial.print("  ");
+		//Serial.print(pulses[i] - start);
+		//Serial.print(' ');
+		//Serial.print(pulses[i+1] - pulses[i]);
+		Serial.println(s);
+		start = pulses[i];
+#endif
+		Serial.println(pulses[i]);
+	}
+
+	//Serial.println();
+}
