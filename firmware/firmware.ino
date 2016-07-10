231a148f9dc4b9d2da749ea4b1415ad414b918a8
Trammell Hudson <hudson@trmm.net>
2016-07-09 23:03:18 -0400

    categorize the pulses into sync, sweep or other

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -10,6 +10,18 @@
  * With the opamp circuit, I found it best to use a 100k feedback on 
  * the first stage and to not include the 220K feedback resistor on
  * the second stage.
+ *
+ * The sync pulses are at 120 Hz, or roughly 8000 usec.  This means that
+ * any pulse *longer* than 8 usec can be discarded since it is not
+ * a valid measurement.
+ *
+ * The sync pulses are sent as one long pulse and one short one.
+ * Since we are only capturing one edge, we see just the short pulse,
+ * of around 400 usec.  If we can't see the lighthouse that this
+ * time slot goes with, we'll see the next sync pulse at 8 usec later.
+ * If we do see this lighthouse, we'll see a sweep pulse at time T,
+ * then roughly 8 usec - T later the next sync.
+ *
  */
 
 #include "InputCapture.h"
@@ -21,6 +33,7 @@
 #define ICP_COUNT 4
 InputCapture icp[ICP_COUNT];
 uint32_t prev[ICP_COUNT];
+boolean saw_sync[ICP_COUNT];
 
 void setup()
 {
@@ -38,6 +51,15 @@ void setup()
 }
 
 
+#if defined(KINETISK)
+#define CLOCKS_PER_MICROSECOND ((double)F_BUS / 1000000.0)
+#elif defined(KINETISL)
+// PLL is 48 Mhz, which is 24 clocks per microsecond, but
+// there is a divide by two for some reason.
+#define CLOCKS_PER_MICROSECOND (F_PLL / 2000000)
+#endif
+
+
 void loop()
 {
 	for(int i = 0 ; i < ICP_COUNT ; i++)
@@ -47,24 +69,54 @@ void loop()
 		if (rc == 0)
 			continue;
 
-		Serial.print(i),
-		Serial.print(',');
+		const uint32_t delta = val - prev[i];
+		prev[i] = val;
+		char type = '?';
+
+		if (rc == -1)
+		{
+			// we lost data.  reset the state
+			type = 'L';
+			saw_sync[i] = 0;
+		} else
+		if (delta < 500 * CLOCKS_PER_MICROSECOND)
+		{
+			// likely a sync pulse, the next one is a sweep
+			type = 'X';
+			saw_sync[i] = val;
+		} else
+		if (delta > 10000 * CLOCKS_PER_MICROSECOND)
+		{
+			// way too long, can't be a real one
+			type = 'J';
+			saw_sync[i] = 0;
+		} else
+		if (delta > 7500 * CLOCKS_PER_MICROSECOND)
+		{
+			// start of another sync pulse, but we didn't
+			// get a sweep.
+			type = 'B';
+			saw_sync[i] = 0;
+		} else
+		if (saw_sync[i])
+		{
+			// we just saw a sync pulse and we aren't out-of-range,
+			// so this is a sweep.
+			type = 'S';
+			saw_sync[i] = 0;
+		} else {
+			// junk pulse.  ignore it
+			type = 'J';
+			saw_sync[i] = 0;
+		}
+
 		Serial.print(val);
 		Serial.print(',');
-
-#if defined(KINETISK)
-#define CLOCKS_PER_MICROSECOND ((double)F_BUS / 1000000.0)
-#elif defined(KINETISL)
-#define CLOCKS_PER_MICROSECOND ((double)F_PLL / 2000000.0)
-#endif
-		//float delta = (val - prev[i]) / CLOCKS_PER_MICROSECOND;
-		//Serial.print(delta);
-
-		Serial.print(val - prev[i]);
+		Serial.print(i),
 		Serial.print(',');
-		Serial.print(rc == -1 ? '0' : '1');
+		Serial.print(type),
+		Serial.print(',');
+		Serial.print(delta);
 		Serial.println();
-
-		prev[i] = val;
 	}
 }
